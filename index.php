8933e5278988b4acb51c2f84741f37105f052a5a
Shubham kakade <57123964+Shubhamkakade06@users.noreply.github.com>
2021-12-03 10:59:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Face Detection</title>
-  <link rel="icon" href="favicon. png" sizes="16x16" type="image/png">
+  <link rel="icon" href="favicon. png" sizes="16x16" type="FD-FAVICON/png">
   <link rel="stylesheet" href="style.css">
 
   <script defer src="face-api.min.js"></script>
